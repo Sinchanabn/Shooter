3279b7c435f0433879d77e3f3b98c71f7f58cff5
TELXSI\sinchana.bn <sincana.bn@tataelxsi.co.in>
2021-11-29 14:28:50 +0530

    Added bullet script and made chnages in playercontroller script

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+
+    float bulSpreed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulSpreed = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Getting the bullet position
+        Vector2 position = transform.position;
+
+        //computing te bullet new position
+        position = new Vector2(position.x, position.y + bulSpreed * Time.deltaTime);
+
+        //updating the bullet position
+        transform.position = position;
+
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        //If the bullet went outside the camera it should destroy
+        if(transform.position.y>max.y)
+        {
+            Destroy(gameObject);
+
+        }
+
+
+
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,85 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    //To store the player movment
     Rigidbody2D rb;
     public float speed;
-    float inputX;
-    float inputY;
-    
 
-    // Start is called before the first frame update
+
+    //Player  prefab game obj
+    public GameObject playerBullet;
+    public GameObject bulletPosition1;
+    public GameObject bulletPosition2;
+    public GameObject bulletPosition3;
+
+    // Start is called before the first frame update*/
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-         inputX = Input.GetAxis("Horizontal");
-         inputY = Input.GetAxis("Vertical");
+
+        //Calling the bullet functon for every 30 secs
+        InvokeRepeating("Shoot", 5f, 30f);
+
+
+        //Accessing the Axis
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        //Calling the move function to compute players position
+        Move(direction);
     }
-    void FixedUpdate()
+
+    //Method to move the player with in camera bonds.
+    void Move(Vector2 direction)
     {
-        rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
-        
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+
+        //Setting up the Max and Min values for player horizontal movment
+
+        max.x = max.x -1.5f;
+        min.x = min.x +1.5f;
+
+        // //Setting up the Max and Min values for player Vertical movment
+        max.y = max.y - 1.5f;
+        min.y = min.y + 1.5f;
+
+        //
+        Vector2 pos = transform.position;
+
+        pos += direction * speed * Time.deltaTime;
+
+        //Clamping the player position
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+
+        transform.position = pos;
+
     }
+    void Shoot()
+    {
+        //Instantiating the first bullet
+        GameObject bullet01 = (GameObject)Instantiate(playerBullet);
+        bullet01.transform.position = bulletPosition1.transform.position;//Set the bullet position
+
+        //Instantiating the Second bullet
+        GameObject bullet02 = (GameObject)Instantiate(playerBullet);
+        bullet02.transform.position = bulletPosition2.transform.position;//Set the bullet position
+
+        //Instantiating the Third bullet
+        GameObject bullet03 = (GameObject)Instantiate(playerBullet);
+        bullet03.transform.position = bulletPosition3.transform.position;//Set the bullet position
+
+    }
+
 }
